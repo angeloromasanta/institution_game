9dfb7f44126d4d53724fc53eb09aec6b70271bf7
Angelo Romasanta <angelokenneth.romasanta@esade.edu>
2025-01-08 16:36:39 +0100

    env correct

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 // App.js
-// App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, onValue, set, get } from 'firebase/database';
@@ -9,6 +8,7 @@ import React, { useState, useEffect } from 'react';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_API_KEY,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
