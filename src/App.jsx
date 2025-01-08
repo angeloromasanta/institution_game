6c57547b8b3c8a6df9eda34464ecef040961f1b5
angeloromasanta <angelokenneth.romasanta@esade.edu>
2025-01-08 15:47:47 +0100

    Firebase edit

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ import React, { useState, useEffect } from 'react';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
